141151e47abc9fa081c73f1f5a3029e5e75db69b
tasneem <tasneem.raneem555@gmail.com>
2019-10-02 15:44:17 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+var showorder = function () {
+    //input
+
+    var userorder = prompt("what would you like to order")
+    var orderImage;
+    //proceesig 
+
+    //out put
+    return;
+
+
+}
+function creatanimals() {
+    var animals = prompt("whats the age?");
+    if  (animals > 6 ){
+    animals = 'big';
+    } else if ( animals<6 ){
+    animals = 'small';
+    }else {
+        animals = "idk";
+    }
+    return animals;
+}
